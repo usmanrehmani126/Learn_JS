a8e1d6ffaf8a70b7fe250a722b352f1acba16103
Usman <usmanrehmani126@gmail.com>
2024-05-29 10:56:18 +0500

    Discuss Let Var Consta

---
+++ LET_VAR_CONST.js
@@ -0,0 +1,9 @@
+console.log("let & const in JS & Temporal Dead Zone")
+console.log(c);
+//when you try to access a variable inside a temporal Dead Zone it gives you a referance Error
+
+const a="a";
+let b="b";
+var c="c";
+
+// console.log(a,b,c);
